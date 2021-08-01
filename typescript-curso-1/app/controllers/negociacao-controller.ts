fab1f272b9969948030593a9acbc0db8abc83e70
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-08-01 15:45:56 -0300

    mensagem view

--- typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -1,6 +1,7 @@
 import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from "../models/negociacao.js";
 import { NegociacoesView } from '../views/negociacoes-view.js';
+import { MensagemView } from '../views/mensagem-view.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -8,6 +9,7 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mesagemView = new MensagemView('#mensagemView');
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -20,6 +22,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mesagemView.update('Negociação adicionada com sucesso');
         this.limparFormulario();
     }
 
