570db920add4027f7bba38f991c616751c0788f1
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-08-05 14:38:40 -0300

    metodos privados

--- typescript-curso-1/app/views/negociacoes-view.ts
+++ typescript-curso-1/app/views/negociacoes-view.ts
@@ -15,9 +15,9 @@ export class NegociacoesView extends View<Negociacoes> {
             </thead>
             <tbody>
             ${model.lista().map(negociacao => {
-                return `
+            return `
                 <tr>
-                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${this.formatar(negociacao.data)}</td>
                     <td>${negociacao.quantidade}</td>
                     <td>${negociacao.valor}</td>
                 </tr>
@@ -26,6 +26,10 @@ export class NegociacoesView extends View<Negociacoes> {
             </tbody>
         </table>
         `;
+    }
+
+    private formatar(data: Date): string {
+        return new Intl.DateTimeFormat().format(data);
     }
 
 }
